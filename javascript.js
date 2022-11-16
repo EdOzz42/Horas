adb3f61a0a07a5f14e7a831751815c127cd7b1cd
Eduardo de Oliveira <eduardoolive133@gmail.com>
2022-11-16 02:45:43 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,17 +1,21 @@
 function carregar() {
-    var msg = window.document.querySelector("div#msg")
-    var img = window.document.querySelector("img#image")
-    var data = new Date()
-    var hourNow = data.getHours()
-    var minutesNow = data.getMinutes()
+    let msg = window.document.querySelector("div#msg")
+    let img = window.document.querySelector("img#image")
+    let data = new Date()
+    let hourNow = data.getHours()
+    let minutesNow = data.getMinutes()
     //var hour = 19
-    msg.innerHTML = `Agora são ${hourNow}:${minutesNow} horas`
+    //var minutesNow = 3
+    
+    if (minutesNow < 10) {
+       minutesNow = '0' + minutesNow 
+    } 
 
-    if (hourNow >= 4 && hourNow < 12) {
+    if (hourNow >= 5 && hourNow < 12) {
         // BOM DIA!
         img.src = "morning.png"
         document.body.style.background = "#E3C474"
-    } else if (hourNow >= 12 && hourNow <= 18) {
+    } else if (hourNow > 5 &&hourNow <= 18) {
         // BUENAS TARDES!
         img.src = "afternoon.png"
         document.body.style.background = "#D38E43"
@@ -20,4 +24,6 @@ function carregar() {
         img.src = "night.png"
         document.body.style.background = "#053430"
     }
+    
+    msg.innerHTML = `Agora são ${hourNow}:${minutesNow} horas`
 }
