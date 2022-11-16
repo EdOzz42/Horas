23b59fe1aa1d7962bed215606d89e2af8fd93a26
Eduardo de Oliveira <eduardoolive133@gmail.com>
2022-11-16 03:26:08 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,9 +2,9 @@ function carregar() {
     let msg = window.document.querySelector("div#msg")
     let img = window.document.querySelector("img#image")
     let data = new Date()
-    //let hourNow = data.getHours()
+    let hourNow = data.getHours()
     let minutesNow = data.getMinutes()
-    var hourNow = 18
+    //var hourNow = 18
     //var minutesNow = 3
     
     if (minutesNow < 10) {
